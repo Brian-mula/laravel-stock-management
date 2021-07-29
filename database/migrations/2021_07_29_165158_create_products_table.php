6447fdbeb03c26416f73806b8eefd6447d3d0280
Brian-mula <mulatiwangilabrian@gmail.com>
2021-07-29 21:12:44 +0300

    third commit

---
+++ database/migrations/2021_07_29_165158_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('supply_id');
+            $table->foreignId('category_id');
+            $table->foreignId('brand_id')->constrained();
+            $table->string('image');
+            $table->string('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
